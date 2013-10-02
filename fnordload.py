6e2f2b069215f32b2888ff2b52ce6cdb84b5742c
Martin Gross <martin@muc.ccc.de>
2013-10-02 22:53:35 +0000

    Using new library for payout

--- fnordload.py
+++ fnordload.py
@@ -1,86 +1,96 @@
 import LCD
 import time
 import eSSP.eSSP
+import max7301
 
 class fnordload(object):
-	def __init__(self, LCDserver = 'localhost', eSSPport = '/dev/ttyACM0',
-			inhibits = [0, 0, 0, 0, 0, 0, 0, 0], cointype = 0.5):
-		self.__LCD = LCD.LCD(LCDserver)
-		self.__eSSP = eSSP.eSSP.eSSP(eSSPport)
-		self.__cointype = cointype
-		self.__inhibits = inhibits
-		self.setup()
+    def __init__(self, LCDserver = 'localhost', eSSPport = '/dev/ttyACM0',
+            inhibits = [0, 0, 0, 0, 0, 0, 0, 0], cointype = 0.5, payoutpin = 6):
+        self.__LCD = LCD.LCD(LCDserver)
+        self.__eSSP = eSSP.eSSP.eSSP(eSSPport)
+        self.__cointype = cointype
+        self.__inhibits = inhibits
+        self.__iodevice = max7301.MAX7301()
+        self.__payoutpin = payoutpin
+        self.setup()
 
-	def setup(self):
-		self.__LCD.setup()
-		self.__eSSP.sync()
-		self.__eSSP.enable_higher_protocol()
-		self.__channelvalues = self.__eSSP.channel_values()[1]
-		self.set_coinlevelinhibits()
+    def setup(self):
+        self.__LCD.setup()
+        self.__eSSP.sync()
+        self.__eSSP.enable_higher_protocol()
+        self.__channelvalues = self.__eSSP.channel_values()[1]
+        self.set_coinlevelinhibits()
+        self.__iodevice.set_pin_as_output(self.__payoutpin)
+        self.__iodevice.set_pin(self.__payoutpin, 1)
 
-	def set_inhibits(self, inhibits = [0, 0, 0, 0, 0, 0, 0, 0]):
-		self.__inhibits = inhibits
-		self.__eSSP.set_inhibits(self.__eSSP.easy_inhibit(inhibits), '0x00')
+    def set_inhibits(self, inhibits = [0, 0, 0, 0, 0, 0, 0, 0]):
+        self.__inhibits = inhibits
+        self.__eSSP.set_inhibits(self.__eSSP.easy_inhibit(inhibits), '0x00')
 
-	def write_coinlevel(self, newlevel):
-		f = open('coins', 'w')
-		f.write(str(newlevel))
-		f.close()
-		self.set_coinlevelinhibits()
+    def write_coinlevel(self, newlevel):
+        f = open('coins', 'w')
+        f.write(str(newlevel))
+        f.close()
+        self.set_coinlevelinhibits()
 
-	def set_coinlevelinhibits(self):
-		f = open('coins', 'r')
-		self.__coins = int(f.read())
-		f.close()
+    def set_coinlevelinhibits(self):
+        f = open('coins', 'r')
+        self.__coins = int(f.read())
+        f.close()
 
-		newinhibits = []
-		for channelvalue, oldinhibit in zip(self.__channelvalues, self.__inhibits):
-			if (self.__coins - (channelvalue / self.__cointype) >= 0 and oldinhibit != 0):
-				newinhibits.append(1)
-			else:
-				newinhibits.append(0)
+        newinhibits = []
+        for channelvalue, oldinhibit in zip(self.__channelvalues, self.__inhibits):
+            if (self.__coins - (channelvalue / self.__cointype) >= 0 and oldinhibit != 0):
+                newinhibits.append(1)
+            else:
+                newinhibits.append(0)
 
-		self.set_inhibits(newinhibits)
-	
-	def payout(self, value):
-		payoutcoins = int(value / self.__cointype)
-		print "Payout of " + str(payoutcoins) + " Coins"
-		self.write_coinlevel(self.__coins - payoutcoins);
-		self.__LCD.payout_in_progress()
-		time.sleep(0.2 * payoutcoins)
-		self.__eSSP.enable()
-		self.welcome()
+        self.set_inhibits(newinhibits)
+    
+    def payout(self, value):
+        payoutcoins = int(value / self.__cointype)
+        print "Payout of " + str(payoutcoins) + " Coins"
+        self.write_coinlevel(self.__coins - payoutcoins);
+        self.__LCD.payout_in_progress()
+        time.sleep(0.2 * payoutcoins)
+        for i in range(0, payoutcoins):
+            self.__iodevice.set_pin(self.__payoutpin, 0)
+            time.sleep(0.1)
+            self.__iodevice.set_pin(self.__payoutpin, 1)
+            time.sleep(0.1)
+        self.__eSSP.enable()
+        self.welcome()
 
-	def welcome(self):
-		self.__LCD.welcome(self.__inhibits, self.__channelvalues)
+    def welcome(self):
+        self.__LCD.welcome(self.__inhibits, self.__channelvalues)
 
-	def main(self):
-		self.welcome()
-		self.__eSSP.enable()
+    def main(self):
+        self.welcome()
+        self.__eSSP.enable()
 
-		while True:
-			poll = self.__eSSP.poll()
+        while True:
+            poll = self.__eSSP.poll()
 
-			if (len(poll) > 1 and len(poll[1]) == 2 and poll[1][0] == '0xef'):
-				if (poll[1][1] == 0):
-					self.__LCD.reading_note()
-				else:
-					self.__eSSP.hold()
-					self.__LCD.reading_note(str(self.__channelvalues[poll[1][1] - 1]))
+            if (len(poll) > 1 and len(poll[1]) == 2 and poll[1][0] == '0xef'):
+                if (poll[1][1] == 0):
+                    self.__LCD.reading_note()
+                else:
+                    self.__eSSP.hold()
+                    self.__LCD.reading_note(str(self.__channelvalues[poll[1][1] - 1]))
 
-			if (len(poll) > 1 and len(poll[1]) == 2 and poll[1][0] == '0xee'):
-				self.__LCD.cashed_note(str(self.__channelvalues[poll[1][1] - 1]))
-				self.__eSSP.disable()
-				self.payout(self.__channelvalues[poll[1][1] - 1])
-			if (len(poll) > 1 and poll[1] == '0xed'):
-				self.__LCD.rejected_note()
-				time.sleep(2)
-				self.welcome()
+            if (len(poll) > 1 and len(poll[1]) == 2 and poll[1][0] == '0xee'):
+                self.__LCD.cashed_note(str(self.__channelvalues[poll[1][1] - 1]))
+                self.__eSSP.disable()
+                self.payout(self.__channelvalues[poll[1][1] - 1])
+            if (len(poll) > 1 and poll[1] == '0xed'):
+                self.__LCD.rejected_note()
+                time.sleep(2)
+                self.welcome()
 
-			time.sleep(0.5)
+            time.sleep(0.5)
 
 if __name__ == "__main__":
-	fnordload = fnordload(inhibits = [1, 1, 1, 1, 1, 1])
+    fnordload = fnordload(inhibits = [1, 1, 1, 1, 1, 1])
 
-	while True:
-		fnordload.main()
+    while True:
+        fnordload.main()
