a1f0f987e4a6d32b597ee728331e52815e42f461
Tobias Schneider <schneider@blinkenlichts.net>
2014-04-22 22:04:51 +0200

    fix(CoinHooper): remove usage of double under

--- fnordload/coin_hopper.py
+++ fnordload/coin_hopper.py
@@ -16,11 +16,11 @@ class CoinHopper(object):
 
     def setup(self):
         self._read_coinlevel()
-        self._iodevice.set_pin_as_output(self.__payoutIn1)
-        self._iodevice.set_pin_as_output(self.__payoutIn2)
-        self._iodevice.set_pin_as_output(self.__payoutIn3)
+        self._iodevice.set_pin_as_output(self._payoutIn1)
+        self._iodevice.set_pin_as_output(self._payoutIn2)
+        self._iodevice.set_pin_as_output(self._payoutIn3)
         self.reset()
-        self._iodevice.set_pin(self.__payoutIn3, 1)
+        self._iodevice.set_pin(self._payoutIn3, 1)
 
     def _write_coinlevel(self, newlevel):
         f = open('coins', 'w')
@@ -47,15 +47,15 @@ class CoinHopper(object):
         self._logger.info("Payout of " + str(payoutcoins) + " Coins")
         self._write_coinlevel(self._coins - payoutcoins);
         for i in range(0, payoutcoins):
-            self._iodevice.set_pin(self.__payoutIn3, 0)
+            self._iodevice.set_pin(self._payoutIn3, 0)
             time.sleep(0.1)
-            self._iodevice.set_pin(self.__payoutIn3, 1)
+            self._iodevice.set_pin(self._payoutIn3, 1)
             time.sleep(0.1)
 
     def reset(self):
-        self._iodevice.set_pin(self.__payoutIn1, 0)
-        self._iodevice.set_pin(self.__payoutIn2, 1)
+        self._iodevice.set_pin(self._payoutIn1, 0)
+        self._iodevice.set_pin(self._payoutIn2, 1)
         time.sleep(0.5)
-        self._iodevice.set_pin(self.__payoutIn1, 1)
-        self._iodevice.set_pin(self.__payoutIn2, 0)
+        self._iodevice.set_pin(self._payoutIn1, 1)
+        self._iodevice.set_pin(self._payoutIn2, 0)
 
