998d2e0c0e175fcc823075a3309662726e9759a6
Tobias Schneider <schneider@blinkenlichts.net>
2014-04-26 23:22:18 +0200

    feat(UI): first shot at an UI class

---
+++ fnordload/ui.py
@@ -0,0 +1,27 @@
+import keypad
+
+class UI(object):
+    def __init__(self, io_device):
+        self._keypad = keypad.KeyPad(io_device)
+    
+    def choose(self, message, options):
+        if len(options) < 1 or len(options) > 3:
+            raise ValueError('Need between 1 and 3 options')
+        
+        count = len(options)
+        options.extend([''] * 3 - count) 
+
+        self._lcd.write(message, options[0], options[1], options[2])
+        
+        while True:
+            choice = self._keypad.get_single_key()
+
+            if choice in range(1, count + 1):
+                return options[choice - 1]
+
+import max7301
+if __name__ == "__main__":
+    iodevice = max7301.MAX7301()
+    ui = UI(io_device = io_device)
+    ui.choose('message', ['foo', 'bar'])
+
