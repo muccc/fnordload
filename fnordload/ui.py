f87ce9991cadae2f08988568d401ca39c41522b3
Tobias Schneider <schneider@blinkenlichts.net>
2014-05-20 21:57:06 +0200

    refact(fnordload, ui): more felxible ui subsystem

--- fnordload/ui.py
+++ fnordload/ui.py
@@ -3,7 +3,7 @@ class UI(object):
         self._lcd = lcd
         self._keypad = keypad
 
-    def choose(self, message, options):
+    def _choose(self, message, options):
         visible_options = [option for option in options if option['visible']]
         visible_count = len(visible_options)
 
@@ -29,6 +29,51 @@ class UI(object):
             key = self._keypad.get_single_key()
             if key in functions:
                 return functions[key]()
+
+    def choose(self, message, options):
+        visible_options = [option for option in options if option['visible']]
+        visible_count = len(visible_options)
+
+        invisible_options = [option for option in options if not option['visible']]
+
+        #defined_keys = [option['key'] for option in options if 'key' in option]
+
+        if visible_count < 1:
+            raise ValueError('Need at least one visible option')
+
+        offset = 0
+
+        while True:
+            if len(visible_options[offset:offset+4]) > 3:
+                show_options = visible_options[offset:offset+2]
+                key = 1
+                for index in range(len(show_options)):
+                    show_options[index]['key'] = index + 1
+                show_options.append({'name': "More...", 'function': lambda: '#', 'visible': True, 'key': '#'})
+
+                if offset > 0:
+                    show_options.append({'name': "back", 'function': lambda: '*', 'visible': False, 'key': '*'})
+                    show_options += [option for option in invisible_options if option['key'] != '*']
+                else:
+                    show_options += invisible_options
+            else:
+                show_options = visible_options[offset:offset+3]
+                for index in range(len(show_options)):
+                    show_options[index]['key'] = index + 1
+                if offset > 0:
+                    show_options.append({'name': "back", 'function': lambda: '*', 'visible': False, 'key': '*'})
+                    show_options += [option for option in invisible_options if option['key'] != '*']
+                else:
+                    show_options += invisible_options
+
+            key = self._choose(message, show_options)
+
+            if key == '#':
+                offset += 2
+            elif key == '*' and offset > 0:
+                offset -= 2
+            else:
+                return key
 
     def input_number(self, value, update_function):
         while True:
